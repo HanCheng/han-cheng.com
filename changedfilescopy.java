3cb483fd692b388087832bc3a9059c4961d4a732
Cheng Han <chan@tripadvisor.com>
2014-10-17 17:54:17 -0400

    add a new java file
    change 4:44
    
    change on 4:45

---
+++ changedfilescopy.java
@@ -0,0 +1,17 @@
+package com.example.chan.testmissingstringview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+add a test file
+
+what a fucking day
+	
+	
+	haha chang 4:44
+		
+		
+		chahahaha change 4:45
+			
